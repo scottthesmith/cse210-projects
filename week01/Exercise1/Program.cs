f394e92d28887583d7e8108f85e94e52c9caef44
scottthesmith <scottthebot@gmail.com>
2026-07-02 23:44:17 -0700

    Added extra strings to the program

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,7 +6,9 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
         Console.WriteLine("I'm writing my very first C# string! What do you think?");
-        Console.ReadLine();
+        Console.WriteLine("What is your favorite TV show?");
+        string show  = Console.ReadLine();
+        Console.WriteLine(show);
     
     }
 }
