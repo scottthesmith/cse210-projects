d28f1588ac40e250309ab6e77e599e28c33f5e55
scottthesmith <scottthebot@gmail.com>
2026-07-02 23:35:00 -0700

    Added a question and a ReadLine

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("I'm writing my very first C# string! What do you think?");
+        Console.ReadLine();
+    
     }
 }
