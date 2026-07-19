762b6de865aca3e326fabf4cb0c4f659034f23cc
scottthesmith <scottthebot@gmail.com>
2026-07-18 17:28:48 -0700

    Completed the basic program requirements.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,14 +1,23 @@
-using System;
-using System.Configuration.Assemblies;
-using System.Security.Cryptography.X509Certificates;
+
 
 class Program
 {
     static void Main(string[] args)
     {
+        
         Reference newReference = new Reference("John", 3, 16);
     
         Scripture chosenScripture = new Scripture(newReference, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        string userChoice = "";
+        while (userChoice != "quit" && chosenScripture.IsCompletelyHidden() == false)
+        {
         Console.WriteLine(chosenScripture.GetDisplayText());
+        Console.WriteLine("Press Enter to hide a random word, or type 'quit' to exit.");
+        userChoice = Console.ReadLine();
+        if (userChoice != "quit")
+        {
+        chosenScripture.HideRandomWords(1);
+        }
+        }
     }
 }
