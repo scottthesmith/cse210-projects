7e665ee6f01b67f37a30235cc26dab2979aad495
scottthesmith <scottthebot@gmail.com>
2026-07-25 07:33:56 -0700

    Updated my vid variables.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -5,9 +5,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Video _vid1 = new Video("Risk of Rain 2: Full playthrough", "ScottPlayzGamezzz",215);
-        Video _vid2 = new Video("How to enter a new world via toilet pipes", "M. Brother", 360);
-        Video _vid3 = new Video("Godzilla: Friend or Foe?", "Kai Ju", 445);
+        Video vid1 = new Video("Risk of Rain 2: Full playthrough", "ScottPlayzGamezzz",215);
+        Video vid2 = new Video("How to enter a new world via toilet pipes", "M. Brother", 360);
+        Video vid3 = new Video("Godzilla: Friend or Foe?", "Kai Ju", 445);
 
         Comment vid1Comment1 = new Comment("Billy Bramble", "Such a great game");
         Comment vid1Comment2 = new Comment("Hugh Huggum", "I really love playing as The Huntress!");
@@ -22,22 +22,22 @@ class Program
         Comment vid3Comment3 = new Comment("Ro Dan", "I could take him.");
 
 
-        _vid1.AddComment(vid1Comment1);
-        _vid1.AddComment(vid1Comment2);
-        _vid1.AddComment(vid1Comment3);
+        vid1.AddComment(vid1Comment1);
+        vid1.AddComment(vid1Comment2);
+        vid1.AddComment(vid1Comment3);
 
-        _vid2.AddComment(vid2Comment1);
-        _vid2.AddComment(vid2Comment2);
-        _vid2.AddComment(vid2Comment3);
+        vid2.AddComment(vid2Comment1);
+        vid2.AddComment(vid2Comment2);
+        vid2.AddComment(vid2Comment3);
 
-        _vid3.AddComment(vid3Comment1);
-        _vid3.AddComment(vid3Comment2);
-        _vid3.AddComment(vid3Comment3);
+        vid3.AddComment(vid3Comment1);
+        vid3.AddComment(vid3Comment2);
+        vid3.AddComment(vid3Comment3);
 
         List<Video> videoList = new List<Video>();
-        videoList.Add(_vid1);
-        videoList.Add(_vid2);
-        videoList.Add(_vid3);
+        videoList.Add(vid1);
+        videoList.Add(vid2);
+        videoList.Add(vid3);
 
         foreach (Video video in videoList)
         {
